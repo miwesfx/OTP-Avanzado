c4d8bd57d5bd8b02c94005a90f90ced14d0445d6
miwesfx <Miguel@macbook-air.local>
2016-05-06 13:06:35 +0200

    OTP y prez añadidos. No funciona el botón que debería llevar a ellos. Botones superpuestos en la primera pantalla

---
+++ OTP Avanzado/InicioVC.swift
@@ -0,0 +1,24 @@
+//
+//  InicioVC.swift
+//  OTP Avanzado
+//
+//  Created by Miguel on 24/4/16.
+//  Copyright © 2016 Miguel Pérez Prado. All rights reserved.
+//
+
+import UIKit
+
+class InicioVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ OTP Avanzado/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OTP Avanzado
+//
+//  Created by Miguel on 24/4/16.
+//  Copyright © 2016 Miguel Pérez Prado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
